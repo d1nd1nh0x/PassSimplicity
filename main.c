e4b16300edfbba88d2e06784eddb3034b9f5f60e
D1nD1nh0x <d1nd1nh0x.69@gmail.com>
2025-01-02 10:41:13 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// defines
+#define MAX_LENGTH 16
+#define MIN_LENGTH 8
+
+// structs
+typedef struct {
+
+  char newPassword[MAX_LENGTH + 1]; // 16 + 1 ('\0')
+  int hasSpecialSymbols;
+  int hasNumeric;
+  int hasSmallLetter;
+  int hasCapitalLetter;
+
+} password;
+
+
+// procedimentos e funções
+
+
+
+int main (void) {
+
+  
+
+  return 0;
+}
